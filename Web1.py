004a52e790cecac105514c2aa7c88893a26d2f8b
jhlimc <jhlimc83@naver.com>
2021-09-30 11:57:01 +0900

    크롤링 연습

--- Web1.py
+++ Web1.py
@@ -10,4 +10,23 @@ page = open("c:\\work\\test01.html","rt", encoding="utf-8").read()
 #검색이 용이한 객체 생성 : 정해진 문자열(html문서, xml은 주로 데이터 저장과 교환)
 soup = BeautifulSoup(page, "html.parser")
 #전체 페이지를 보기
-print(soup.prettify())
+#print(soup.prettify())
+
+#문서 내부에 <b>태그 몽땅
+#print(soup.find_all("b"))
+
+#문서 내부에 <p> 모두 검색 :List로 담아서 리턴
+#print(soup.find_all("p"))
+
+#첫번째 <p>를 가져오기
+#print(soup.find("p"))
+
+#조건이 있는 경우 : <p class="outer-text">
+#print(soup.find_all("p", class_="outer-text"))
+
+#태그 안쪽에 문자열을 검색:  .text 속성을 사용
+for tag in soup.find_all("p"):
+    title = tag.text
+    title2 = title.replace("\n","")
+    title2 = title2.replace("\t","")
+    print(title.strip())
